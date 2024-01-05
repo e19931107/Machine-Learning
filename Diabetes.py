d0a24aab77d76925326464f65adb8f9cb1450e8d
Edward <50692450+e19931107@users.noreply.github.com>
2024-01-05 10:36:55 +0800

    Integrate file and edit README

--- Diabetes.py
+++ Diabetes.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn import datasets
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import mean_squared_error as mse
 import matplotlib.pyplot as plt
 
 db = datasets.load_diabetes()
@@ -32,3 +34,42 @@ plt.xlabel("Quantitative Measure")
 plt.ylabel("Predicted Quantitive Measure")
 plt.title("Quantitative Measure vs Predicted Quantitative Measure")
 plt.show()
+
+#----------------------------------------------------------------------
+
+x = pd.DataFrame(db.data, columns = db.feature_names)
+y = pd.DataFrame(db.target, columns = ["Test"])
+
+#建模
+lm = LinearRegression() 
+lm.fit(x, y)
+pred_train = lm.predict(x)
+MSE_train = mse(y, pred_train)
+print('-'*30)
+print('MSE:',MSE_train)
+print('R-square:',lm.score(x, y))
+
+#----------------------------------------------------------------------
+
+XTrain_1, XTest_1, yTrain_1, yTest_1 = tts(x, y, test_size = 0.25, random_state = 100)
+lm = LinearRegression() 
+lm.fit(XTrain_1, yTrain_1)
+pred_test_1 = lm.predict(XTest_1)
+MSE1 = mse(yTest_1, pred_test_1)
+
+print('-'*30)
+print('MSE1:',MSE1)
+print(f'MSE1\'s R-square:{lm.score(XTest_1, yTest_1)}')
+
+#----------------------------------------------------------------------
+
+XTrain_2, XTest_2, yTrain_2, yTest_2 = tts(x, y, test_size = 0.2, random_state = 100)
+lm = LinearRegression() 
+lm.fit(XTrain_2, yTrain_2)
+pred_test_2 = lm.predict(XTest_2)
+MSE2 = mse(yTest_2, pred_test_2)
+
+print('-'*30)
+print('MSE2:',MSE2)
+print(f'MSE2\'s R-square:{lm.score(XTest_2, yTest_2)}')
+
