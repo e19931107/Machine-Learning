01299d1da1d8a1b4dafaf822b3c0891a296715d0
Edward <50692450+e19931107@users.noreply.github.com>
2024-01-08 20:00:17 +0800

    加一點註解

--- Diabetes.py
+++ Diabetes.py
@@ -11,6 +11,7 @@ db = datasets.load_diabetes()
 #print(db.feature_names)
 
 x_1 = pd.DataFrame(db.data, columns = db.feature_names)
+#取age (年齡)、 sex (性別)、 bmi (Body Mass Index 體質指數)、bp (Average Blood Pressure 平均血壓)做訓練
 x_2 = x_1.iloc[:,0:4]
 y = pd.DataFrame(db.target, columns = ["Predicted Quantitatjve Measure"])
 
@@ -21,14 +22,14 @@ lm_2 = LinearRegression()
 lm_2.fit(x_2,y)
 
 #畫圖
-pre_tar1 = lm_1.predict(x_1) 
+pre_tar1 = lm_1.predict(x_1) #根據x_1（所有資料），也就是用所有資料去預測
 plt.scatter(y, pre_tar1)
 plt.xlabel("Quantitative Measure")
 plt.ylabel("Predicted Quantitive Measure")
 plt.title("Quantitative Measure vs Predicted Quantitative Measure")
 plt.show()
 
-pre_tar2 = lm_2.predict(x_2)
+pre_tar2 = lm_2.predict(x_2) #根據x_2（age, sex, bmi, bp）預測
 plt.scatter(y, pre_tar2)
 plt.xlabel("Quantitative Measure")
 plt.ylabel("Predicted Quantitive Measure")
@@ -50,7 +51,7 @@ print('MSE:',MSE_train)
 print('R-square:',lm.score(x, y))
 
 #----------------------------------------------------------------------
-
+# 設定資料集分割為0.25，也就是拿3份資料去訓練，預測1份的資料
 XTrain_1, XTest_1, yTrain_1, yTest_1 = tts(x, y, test_size = 0.25, random_state = 100)
 lm = LinearRegression() 
 lm.fit(XTrain_1, yTrain_1)
